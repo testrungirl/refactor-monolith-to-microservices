b0b1f0d70c62609670996a56613f75eb99f8a992
testrungirl <laigbodioh@gmail.com>
2021-09-04 15:29:28 +0000

    Task update: includes docker builds using travis file

---
+++ deployment/deploy-docker.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push testrungirl/udacity-restapi-user
+docker push testrungirl/udacity-restapi-feed
+docker push testrungirl/reverseproxy
+docker push testrungirl/udacity-frontend:local
