190c63fa7e4cd83910cdc1f114792e479ac2fdcb
testrungirl <laigbodioh@gmail.com>
2021-09-05 02:57:01 +0000

    edited Docker verision in travis.yml file

--- deployment/deploy-docker.sh
+++ deployment/deploy-docker.sh
@@ -4,4 +4,4 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push testrungirl/udacity-restapi-user
 docker push testrungirl/udacity-restapi-feed
 docker push testrungirl/reverseproxy
-docker push testrungirl/udacity-frontend:local
+docker push testrungirl/udacity-frontend
