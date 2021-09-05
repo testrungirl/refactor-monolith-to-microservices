e699b48351f2a87b44efb18c4de686aea76015f1
testrungirl <laigbodioh@gmail.com>
2021-09-05 05:05:02 +0000

    edited docker image links

--- deployment/deploy-docker.sh
+++ deployment/deploy-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push testrungirl/udagram-restapi-user
-docker push testrungirl/udagram-restapi-feed
+docker push testrungirl/udagram-api-user
+docker push testrungirl/udagram-api-feed
 docker push testrungirl/reverseproxy
 docker push testrungirl/udagram-frontend:local
